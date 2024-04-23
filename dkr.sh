a2f11c69fa3af80d6adf67ade5ecf61671b4562f
Caleb Forestal | <133164966+Calebrlx@users.noreply.github.com>
2024-04-22 16:45:31 -0800

    Update dkr.sh

--- dkr.sh
+++ dkr.sh
@@ -44,14 +44,6 @@ echo "Docker Compose version: $(docker-compose --version)"
 
 
 
-
-# sudo apt install apt-transport-https ca-certificates curl software-properties-common
-
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-# sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-# sudo apt update
-# sudo apt install docker-ce
-
 # sudo systemctl start docker
 # sudo systemctl enable docker
 
